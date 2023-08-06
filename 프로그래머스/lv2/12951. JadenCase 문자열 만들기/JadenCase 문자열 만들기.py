36e1100e64023e879f74b15844c5af89de4f3ce5
dudwhd20 <dudwhd20@naver.com>
2023-08-06 16:41:44 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,9 @@
+def solution(s):
+    a = s.split(" ")
+    arr = []
+    for _ in a:
+        if(_):
+            arr.append(_[0].upper() + _[1:].lower())
+        else:
+            arr.append(_)
+    return ' '.join(arr)
