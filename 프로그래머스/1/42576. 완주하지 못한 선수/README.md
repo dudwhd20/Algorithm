e1a586c3d0f6397f58c828109c81c908108ea211
dudwhd20 <dudwhd20@naver.com>
2024-02-07 15:00:13 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 88.44 ms, Memory: 95 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
 
 ### 성능 요약
 
-메모리: 34 MB, 시간: 40.21 ms
+메모리: 95 MB, 시간: 88.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 21:46:10
+2024년 2월 3일 15:0:10
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String,Integer> m = new HashMap<>();
+        
+        for(String s : participant){
+            m.put(s, m.getOrDefault(s,0) + 1);    
+        }
+        
+        for(String s : completion){
+            m.put(s, m.get(s) - 1);    
+        }
+        
+        for(String k : m.keySet()){
+            if(m.get(k) != 0){
+                answer = k;
+            }
+        }
+        
+        return answer;
+    }
+}
