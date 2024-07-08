94d387ff0205c09204911fd8deb90cc22849b6da
소영종 <dudwhd20@naver.com>
2024-07-08 20:24:09 +0900

    [level 2] Title: 조건에 맞는 도서와 저자 리스트 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/조건에 맞는 도서와 저자 리스트 출력하기.sql
@@ -0,0 +1,6 @@
+SELECT b.book_id, a.author_name, date_format(b.published_date, '%Y-%m-%d')
+from book b
+left join author a
+on b.author_id = a.author_id
+where b.category = '경제'
+order by b.published_date
