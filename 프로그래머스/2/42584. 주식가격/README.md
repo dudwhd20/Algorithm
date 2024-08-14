40136461c847d81274eead4af5592cd77bf94c69
soyj <dudwhd20@naver.com>
2024-08-14 22:22:19 +0900

    [level 2] Title: 주식가격, Time: 36.21 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=java) 
 
 ### 성능 요약
 
-메모리: 16.8 MB, 시간: 35.61 ms
+메모리: 73.3 MB, 시간: 36.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 24일 14:56:07
+2024년 08월 14일 22:22:15
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        
+        for(int i=1; i<prices.length; i++){
+            
+            while(!stack.isEmpty() && prices[i] < prices[stack.peek()]){
+                int j = stack.pop();
+                answer[j] = i - j;
+            }
+            stack.push(i);
+        }
+        while(!stack.isEmpty()){
+            int j = stack.pop();
+            answer[j] = prices.length - 1 - j;
+        }
+        return answer;
+    }
+}
