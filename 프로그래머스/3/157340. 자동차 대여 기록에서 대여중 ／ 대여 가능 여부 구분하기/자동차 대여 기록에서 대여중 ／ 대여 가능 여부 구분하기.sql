7883c0d402cc895ca89e5135c39e38c98486cb08
soyj <dudwhd20@naver.com>
2024-09-29 14:41:30 +0900

    [level 3] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -0,0 +1,12 @@
+SELECT
+    CAR_ID,
+    CASE
+        WHEN max('2022-10-16' BETWEEN START_DATE AND END_DATE) = 1 THEN '대여중'
+        ELSE '대여 가능'
+    END AS AVAILABILITY
+FROM
+    CAR_RENTAL_COMPANY_RENTAL_HISTORY
+group by
+    CAR_ID
+order by
+    CAR_ID desc;
