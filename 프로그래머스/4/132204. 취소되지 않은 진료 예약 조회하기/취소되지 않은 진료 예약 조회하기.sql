8705f3567770b906fc35d3121a98a0bf375e3fa2
soyj <dudwhd20@naver.com>
2024-09-30 22:21:35 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -0,0 +1,16 @@
+SELECT  A.APNT_NO
+        ,P.PT_NAME
+        ,P.PT_NO
+        ,A.MCDP_CD
+        ,D.DR_NAME
+        ,A.APNT_YMD
+  FROM  APPOINTMENT AS A
+  JOIN  PATIENT AS P
+    ON  A.PT_NO = P.PT_NO
+  JOIN  DOCTOR AS D
+    ON  D.DR_ID = A.MDDR_ID
+ WHERE  A.MCDP_CD = 'CS' 
+        AND DATE_FORMAT(A.APNT_YMD, '%Y-%m-%d') = '2022-04-13'
+        AND APNT_CNCL_YN = 'N'
+ ORDER
+    BY  APNT_YMD
