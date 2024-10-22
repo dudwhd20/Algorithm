680cd221a74588b399011989f86cdf33e93af2b5
soyj <dudwhd20@naver.com>
2024-10-22 22:29:06 +0900

    [level 2] Title: 구명보트, Time: 11.43 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=java) 
 
 ### 성능 요약
 
-메모리: 10.8 MB, 시간: 9.36 ms
+메모리: 53.8 MB, 시간: 11.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 11일 14:33:18
+2024년 10월 22일 22:29:02
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int n = people.length;
+        Arrays.sort(people);
+
+        int i = n - 1;
+        int j = 0;
+
+        while (i > j) {
+            if (people[i] + people[j] <= limit) {
+                i--;
+                j++;
+            }
+            else {
+                i--;
+            }
+        }
+
+        return answer = (n - j);
+    }
+}
