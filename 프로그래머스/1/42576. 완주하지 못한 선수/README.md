559577f88f5069e825ba8de1131b7c3f829c34e9
soyj <dudwhd20@naver.com>
2025-01-01 17:04:36 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 105.31 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 34 MB, 시간: 46.02 ms
+메모리: 97.3 MB, 시간: 105.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 15일 09:07:17
+2025년 01월 01일 17:04:34
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,25 +1,22 @@
 import java.util.*;
-
-
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         Map<String,Integer> m = new HashMap<>();
         
         for(String s : participant){
-            m.put(s, m.getOrDefault(s,0) + 1);    
+            m.put(s, m.getOrDefault(s, 0) + 1);
         }
         
         for(String s : completion){
-            m.put(s, m.get(s) - 1);    
-        }
+            m.put(s, m.get(s) - 1);
+        }        
         
         for(String k : m.keySet()){
             if(m.get(k) != 0){
                 answer = k;
             }
         }
-        
         return answer;
     }
 }
