cc16dc008982a67941d593ce3ca7858798365b8e
soyj <dudwhd20@naver.com>
2025-06-05 14:20:13 +0900

    [level 1] Title: 예산, Time: 1.21 ms, Memory: 91.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        
+        for(int i : d){
+            if(budget >= i){
+                budget -= i;
+                answer++;                
+            }
+        }
+        return answer;
+    }
+}
